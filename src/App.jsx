0b71b0290f5ef80a948afcb96002f0723784a7e7
aBushara <moj.mail@mail.com>
2023-01-18 19:29:18 +0100

    Added form for adding new users

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { NewUser, User } from "./user";
+
+export default class App extends React.Component {
+    state = {
+        users: [
+            { id: 1, name: "Ivan", years: 30 },
+            { id: 2, name: "Marko", years: 35 },
+            { id: 3, name: "Ana", years: 25 },
+        ],
+    }
+
+    handleButtonPress = () => {
+        const { users } = this.state;
+
+        const newUsers = users.map((user) => {
+            return { ...user, years: user.years + 1 };
+        });
+
+        this.setState({ users: newUsers });
+    };
+
+    handleNameChange = (event, index) => {
+        const { users } = this.state;
+        const newUsers = [...users];
+
+        newUsers[index].name = event.target.value;
+        this.setState({ users: newUsers });
+    };
+
+    handleAddUser = (addedUser) => {
+        const { users } = this.state;
+        this.setState({
+            users: [...users, addedUser]
+        });
+    };
+
+    render() {
+        const { users } = this.state;
+
+        return (
+            <div>
+                <h1>React aplikacija</h1>
+                <p>Ovo zbilja radi</p>
+                <button onClick={this.handleButtonPress}>
+                    Promjena godina
+                </button>
+
+                {users.map((user, index) => (
+                    <User
+                        key={user.id}
+                        name={user.name}
+                        years={user.years}
+                        onNameChange={(event) =>
+                            this.handleNameChange(event, index)
+                        }
+                    />
+                ))}
+                <NewUser
+                    id={users[users.length - 1].id}
+                    addUser={this.handleAddUser} 
+                />
+            </div>
+        );
+    }
+}
---
+++ src/user/NewUser.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+
+class NewUser extends Component {
+    state = {
+        id: 0,
+        name: '',
+        years: ''
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const { name, years } = this.state;
+        if (!name || !years) {
+            alert("Upiši ime i/ili dob!");
+            return;
+        }
+
+        this.props.addUser(this.state);
+
+        this.setState({
+            name: '',
+            years: ''
+        });
+    }
+
+
+    handleSaveUser = (e) => {
+        this.setState({
+            id: this.props.id + 1,
+        });
+
+        switch (e.target.id) {
+            case "name":
+                this.setState({
+                    name: e.target.value,
+                });
+                break;
+            case "years":
+                this.setState({
+                    years: parseInt(e.target.value),
+                });
+                break;
+            default:
+                break;
+        }
+    };
+
+    render() {
+        const {name, years} = this.state;
+        return (
+            <>
+                <h4>Novi korisnik</h4>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Ime: </label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        autoComplete="off"
+                        value={name}
+                        onChange={this.handleSaveUser}
+                    />
+                    <br />
+                    <label htmlFor="years">Dob: </label>
+                    <input
+                        type="text"
+                        id="years"
+                        name="years"
+                        autoComplete="off"
+                        value={years}
+                        onChange={this.handleSaveUser}
+                    />
+                    <br />
+                    <button>Dodaj</button>
+                </form>
+            </>
+        );
+    }    
+}
+
+export default NewUser;
